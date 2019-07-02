76d02e7b729c033e69a1f322a17a479cd6dde23b
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-07-02 13:11:36 +0530

    Home: Build home page UI
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
