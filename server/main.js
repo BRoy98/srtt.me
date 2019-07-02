e1e1d217df05bbf56830aec7d181f26edd3381fd
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-07-03 01:53:35 +0530

    Server: add frontend routes and serve static files
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- server/main.js
+++ server/main.js
@@ -7,6 +7,10 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
 const config = require('./config');
+const cors = require('cors');
+const {
+    preservedUrls
+} = require('./util');
 
 // Establish Connection
 mongoose.connect(config.MONGO_URL, {
@@ -20,16 +24,25 @@ db.once('open', function () {
 });
 
 // Security headers
+app.use(cors());
 app.set('trust proxy', true);
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 app.use((req, res, next) => {
     req.realIp = req.headers['x-real-ip'] || req.connection.remoteAddress || '';
     return next();
+});
+
+// serve frontend
+app.use(express.static(path.join(__dirname + '/../client/dist/srtt')));
+
+// frontend routes
+app.get(preservedUrls, (req, res) => {
+    res.sendFile(path.resolve('client/dist/srtt/index.html'));
 });
 
 app.listen(config.PORT, err => {
---
+++ server/util/index.js
@@ -0,0 +1,27 @@
+exports.generateUrl = (count) => {
+    if (!count)
+        count = 6;
+    var date = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+        date += performance.now(); //use high-precision timer if available
+    }
+
+    var url = '';
+    for (i = 0; i < count; i++) {
+        url += 'x';
+    }
+    return url.replace(/[xy]/g, function (c) {
+        var r = (date + Math.random() * 16) % 16 | 0;
+        date = Math.floor(date / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+};
+
+exports.preservedUrls = [
+    'login',
+    'contact',
+    '404',
+    'verify',
+    'terms',
+    'privacy',
+];
