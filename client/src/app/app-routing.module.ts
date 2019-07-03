0b74593fc366af7912360208e1640a1106631ec2
Bishwajyoti Roy <bishwajyoti456@gmail.com>
2019-07-03 20:32:13 +0530

    View: Add sign in, contact and 404 error page
    Signed-off-by: Bishwajyoti Roy <bishwajyoti456@gmail.com>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: 'signin',
+    component: SigninComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent,
   },
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,19 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from 'ngx-clipboard';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent,
+    SigninComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
