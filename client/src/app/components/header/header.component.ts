d242935b7838184a6102e9d361b86e3090a562bf
BRoy98 <bishwajyoti456@gmail.com>
2019-08-10 00:00:45 +0530

    Heder: Fix signin button to work as sign out when user is logged in
    Signed-off-by: BRoy98 <bishwajyoti456@gmail.com>

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { NetworkService } from 'src/app/services/network.service';
 import { EventListenerService } from 'src/app/services/event-listener.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,28 +13,46 @@ export class HeaderComponent implements OnInit {
 
   @Output() signInEvent = new EventEmitter<boolean>();
   @Input() signInInput: boolean;
-  isSignedIn = false;
+  public signInText = "Sign in / Join";
+  public currentRoute = "";
+  public routeSubscription;
 
   constructor(
     private router: Router,
     private networkService: NetworkService,
-    private eventListener: EventListenerService) { }
+    private eventListener: EventListenerService) {
+
+    this.routeSubscription = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = this.router.url;
+      if (this.currentRoute === "/dashboard") {
+        this.signInText = "Sign out";
+      } else if (this.currentRoute !== "/dashboard" && this.networkService.isAuthenticated()) {
+        this.signInText = "My Account";
+      } else {
+        this.signInText = "Sign in / Join";
+      }
+    });
+  }
 
   ngOnInit() {
-    if (this.networkService.isAuthenticated()) {
-      this.isSignedIn = true;
-    }
     this.eventListener.loginChange.subscribe(() => {
       if (this.networkService.isAuthenticated()) {
-        this.isSignedIn = true;
+        this.router.navigate(['/dashboard']);
         this.signInInput = !this.signInInput;
         this.signInEvent.emit(this.signInInput);
+      } else {
+        this.signInText = "Sign in / Join";
       }
     });
   }
 
   toggleSignIn() {
-    if (!this.networkService.isAuthenticated()) {
+    if (this.currentRoute === "/dashboard") {
+      this.networkService.logout();
+      this.router.navigate(['/']);
+    } else if (!this.networkService.isAuthenticated()) {
       this.signInInput = !this.signInInput;
       this.signInEvent.emit(this.signInInput);
     } else {
