4bf6b51cdaf9e535c5619b3d249d1b46a25d5c39
BRoy98 <bishwajyoti456@gmail.com>
2019-08-09 23:48:52 +0530

    Add user dashbaord - user can set custom short URL (optional) - show user urls on the dashboard
    Signed-off-by: BRoy98 <bishwajyoti456@gmail.com>

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NetworkService } from 'src/app/services/network.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+  urlData = {
+    url: '',
+    shorturl: ''
+  }
+
+  public warnMsg = '';
+
+  public urlSubmitting = false;
+  public urlListLoading = false;
+  public userUrls = [];
+
+  constructor(
+    private networkService: NetworkService) { }
 
   ngOnInit() {
+    this.urlListLoading = true;
+    this.networkService.getUrls().subscribe(
+      res => {
+        this.userUrls = res.urls;
+        this.urlListLoading = false;
+      },
+      err => {
+        // this.warnMsg = err.error;
+        this.urlListLoading = false;
+      }
+    );
+  }
+
+  submitPress() {
+    if (this.urlSubmitting)
+      return;
+
+    this.warnMsg = "";
+    this.urlSubmitting = true;
+    this.networkService.addUrl(this.urlData).subscribe(
+      res => {
+        this.userUrls.push(res.urlData);
+        this.urlData.url = "";
+        this.urlData.shorturl = "";
+        this.urlSubmitting = false;
+      },
+      err => {
+        this.warnMsg = err.error.error;
+        this.urlSubmitting = false;
+      }
+    );
   }
 
 }
